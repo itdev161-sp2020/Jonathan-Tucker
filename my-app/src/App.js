56908ca0e56d95abd088db6ffaf51138056797ea
Jonathan <tuckej18@gmatc.matc.edu>
2020-04-28 21:18:17 -0500

    Completed Activity 7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
  class App extends React.Component{
    state = {
@@ -19,13 +23,34 @@ import axios from 'axios';
      })
    }
    render(){
-     return(
-       <div className="App">
-         <header className="App-header">
-           GoodThings
-         </header>
-         {this.state.data}
-       </div>
+     return (
+       <Router>
+         <div className="App">
+           <header className="App-header">
+             <h1> GoodThings</h1>
+             <ul>
+               <li>
+                 <Link to="/">Home</Link>
+               </li>
+               <li>
+                 <Link to="/register">Register</Link>
+                 <li>
+                   <Link to="/login">Login</Link>
+                 </li>
+               </li>
+             </ul>
+           </header>
+           <main>
+             <Route exact path="/">
+               {this.state.data}
+             </Route>
+             <Switch>
+               <Route path="/register" component={Register} />
+               <Route exact path="/Login" component={Login} /> 
+             </Switch>
+            </main>
+         </div>
+       </Router>
      );
    }
  }
