d7534e2743e8c2942fbf791cc64666f579206126
Jonathan <tuckej18@gmatc.matc.edu>
2020-04-30 17:30:22 -0500

    Activity 7

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ import express from "express";
 import connectDatabase from "./config/db";
 import {check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import User from './models/User';
+import auth from './my-app/src/middleware/auth';
 
 
 // Initialize express application
@@ -38,17 +43,34 @@ app.post('/api/users',
     check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
 ],
 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({errors: errors.array() });
     } else {
-    return res.send(req.body);
+        const {name, email, password} = req.body;
+        try{
+        let user = await User.findOne({email: email});
+        if (user) {
+            return res
+        .status(400)
+        .json({errors: [ {msg: 'user already exists'}]});
     }
+    user = new User({
+        name: name,
+        email: email,
+        password: password
+    });
 
+    
+const salt = await bcrypt.genSalt(10);
+user.password = await bcrypt.hash(password, salt);
+
+await user.save();
+res.send('User successfully registered');
+    }catch (error){
+        res.status(500).send('Server error');
+    }
+  }
 }
 );
-
-// Connection listener
-app.listen(5000, () => console.log('Express server running on part ${port}'));
-
